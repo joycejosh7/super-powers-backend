5bbef7946afd0893d323c4b48cf1f4510e7637b3
Joshua <joycejosh7@gmail.com>
2021-05-04 12:50:46 -0400

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hero.create(secret_identity: "Mark Grayson", super_name: "Invincible")
+Hero.create(secret_identity: "Nolan Grayson", super_name: "Omni-Man")
+Hero.create(secret_identity: "Eve Wilkins", super_name: "Atom Eve")
+
+Power.create(content: "Flight", hero_id: 1)
+Power.create(content: "Strength", hero_id: 1)
+
+Power.create(content: "Flight", hero_id: 2)
+Power.create(content: "Strength", hero_id: 2)
+
+Power.create(content: "Flight", hero_id: 3)
