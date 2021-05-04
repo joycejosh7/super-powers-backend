efe4e5d85d5cf8e33b9148f55d027ecb17b5c7ff
Joshua <joycejosh7@gmail.com>
2021-05-04 14:15:05 -0400

    added index and show

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,11 @@
 class HerosController < ApplicationController
+
+    def index
+        render json: Hero.all
+    end
+
+    def show
+        hero = Hero.find_by(id: params[:id])
+        render json: hero
+    end
 end
